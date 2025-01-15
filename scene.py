a9a590e70ba9a5d033b5aca4270972a7a89d7369
Keith Summers <pikimix@gmail.com>
2025-01-15 21:40:49 +0000

    refactor scene to use new entities, spawn enemies outside of the screen and manage the life cycle of enemies

--- scene.py
+++ scene.py
@@ -11,39 +11,78 @@ logging.basicConfig(level=0)
 class Scene:
     """A scene (or "level") of the game
     """
-    def __init__(self):
+    def __init__(self, bounds: pg.Rect):
         """Create a new scene
         """
+        self.bounds = bounds
         sprite =  {
             'file':'assets/sprites/player.png',
             'width': 32,
             'height': 32,
             'frames': 4
         }
-        psprite = Player((128,128), sprite)
+        psprite = Player(bounds.center, sprite)
         self.player: GroupSingle[Player] = GroupSingle(psprite)
         self.all_sprites: LayeredDirty[Enemy|Player] = LayeredDirty()
         self.enemies: Group[Enemy] = Group()
-        for _ in range(4):
-            loc = (random.randint(0,1200), random.randint(0,700))
-            enemy = Enemy(loc, sprite)
+        for _ in range(10):
+            enemy = Enemy(self.spawn_outside(), sprite)
             enemy.move_towards(pg.Vector2(self.player.sprites()[0].rect.center))
             self.enemies.add(enemy)
         for enemy in self.enemies:
             enemy.tint('Red')
         self.all_sprites.add(self.enemies, layer=1)
         self.all_sprites.add(self.player, layer=2)
+        self.dead_sprites: Group = Group()
 
-    def update(self, bounds: pg.Rect):
-        """Update the current scene
+    def spawn_outside(self) -> tuple:
+        """Provide spawn point thats outside the playable bounds.
 
-        Parameters
-        ----------
-        bounds : pg.Rect
-            The bounds to keep the player within - This may move in future.
+        Returns
+        -------
+        tuple
+            tuple representing a location to spawn thats outside the space of the screen
         """
-        for p in self.player:
-            p.update(bounds)
+        direction = random.choice(['top', 'left', 'right', 'bottom'])
+        # logger.debug('Scene:spawn_outside: direction=%s', direction)
+        offset = [random.randint(50,100), random.randint(0,100)]
+
+        if direction == 'top':
+            offset[0] = random.randint(0,self.bounds.width)
+            offset[1] = -offset[1]
+        if direction == 'left':
+            offset[0] = -offset[0]
+            offset[1] = random.randint(0,self.bounds.height)
+        if direction == 'right':
+            offset[0] += self.bounds.width
+            offset[1] = random.randint(0,self.bounds.height)
+        if direction == 'bottom':
+            offset[0] = random.randint(0,self.bounds.width)
+            offset[1] += self.bounds.height
+        return tuple(offset)
+
+    def update(self):
+        """Update the current scene
+        """
+        self.player.update(self.bounds)
+
+        for e in self.enemies:
+            e.update()
+            if not self.bounds.contains(e.rect):
+                if e.has_been_onscreen:
+                    e.kill()
+                    self.dead_sprites.add(e)
+            elif not e.has_been_onscreen:
+                e.has_been_onscreen = True
+
+        # logger.debug('len dead: %s', len(self.dead_sprites))
+        for d in self.dead_sprites:
+            if isinstance(d, Enemy):
+                d.kill()
+                d.respawn(self.spawn_outside())
+                d.move_towards(self.player.sprites()[0].rect.center)
+                d.add(self.enemies)
+                self.all_sprites.add(d, layer=1)
 
     def draw(self, screen: pg.Surface):
         """Draw the current scene to the provided screen
