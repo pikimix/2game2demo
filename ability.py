34ab74ac8d387f53e18414bbcf2a3e63f8329e42
Keith Summers <pikimix@gmail.com>
2025-01-26 21:06:47 +0000

    update ability to take an actual particle instead of a dict, and to take a base velocity arg

--- ability.py
+++ ability.py
@@ -1,12 +1,13 @@
 """A module for classes relating to player abilities
 """
-
+import pygame as pg
+import particle
 class BaseAttack:
     """Player Basic Attack
     """
 
     def __init__(self, power: int=10, interval: int=500, uses: int=-1, is_super: bool=False,
-                    particle: dict[str,str|int]=None):
+                    base_particle: particle.Particle=None, max_velocity: int=600):
         """Create a new basic attack with the provided details
 
         Parameters
@@ -19,13 +20,16 @@ class BaseAttack:
             The number of times the attack can be used, -1 is infinite, by default -1
         is_super : bool, optional
             If the attack is a super attack, by default False
-        particle : dict[str,str|int], optional
-            Dict which describes the color/ lifetime, max velocity of the particles this 
-            attack generates, by default None
-            None results in {'color':'Yellow', 'lifetime':1000, 'max_vel':600} being used
+        base_particle : particle.Particle
+            A default particle used to create the attack via deepcopy before updating its velocity
+            and spawn time, by default None
+            None results in {'color':'Yellow', 'lifetime':1000, } being used
         """
         self.power = power
         self.interval = interval
         self.uses = uses
         self.is_super = is_super
-        self.particle = particle if particle else {'color':'Yellow', 'lifetime':1000, 'max_vel':600}
+        self.max_velocity = max_velocity
+        self.particle = base_particle if base_particle else particle.Particle(pg.Rect(0,0,1,1),
+                                                                    pg.Vector2(0,0),
+                                                                    'Yellow', 1000)
